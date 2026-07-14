512762e1585d7b02a893fe71b79722b7bebbe0c6
AntoineQuarroz <antoine.quarroz@hes-so.ch>
2026-07-14 23:34:32 +0200

    chore: increment ios build number to 7

--- app.config.ts
+++ app.config.ts
@@ -17,7 +17,7 @@ const config: ExpoConfig = {
   userInterfaceStyle: 'automatic', // Supporte light + dark automatiquement
   ios: {
     bundleIdentifier: IS_PROD ? 'ch.courseo.app' : `ch.courseo.app.${APP_ENV}`,
-    buildNumber: '6',
+    buildNumber: '7',
     supportsTablet: false, // MVP telephone uniquement
     requireFullScreen: true,
     infoPlist: {
